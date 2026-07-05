1658a7a738d35f2cea95e862c330e6015ccecf81
vedu989v-wq <vedu989v@gmail.com>
2026-07-06 01:29:38 +0530

    Third Version

--- student_dashboard/app.js
+++ student_dashboard/app.js
@@ -15,24 +15,78 @@ async function loadStudents(){
     
 }
 
+//PCB of This Application: Store current State
+const state={
+    students:[],
+    search:"",
+    department:"ALL",
+    sort:"none"
+}
 
+function updateUI(){
+    let filtered=[...state.students];
 
-function setupSearch(students){
+    //Search Function
+    if(state.search!==""){
+        filtered=filtered.filter(student=>student
+            .name.toLowerCase()
+            .includes(state.search.toLowerCase())
+        ); //for matching purpose
+    }
+
+    //Department
+    if(state.department!=='ALL'){
+        filtered=filtered.filter(student=>student.department==state.department);
+    }
+
+    
+
+    //sorting
+    if(state.sort!='none'){
+       if(state.sort=="cgpa-asc"){
+         filtered.sort((a,b)=>a.cgpa-b.cgpa);
+       }
+       if(state.sort=="cgpa-desc"){
+         filtered.sort((a,b)=>b.cgpa-a.cgpa);
+       }
+    }
+
+    renderTable(filtered)
+}
+
+//Search Filter
+function setupSearch(){
 
 const searchBox=document.querySelector('.search').querySelector('input')
 
 searchBox.addEventListener('input', ()=>{
-    const keyword=searchBox.value;
-
-    const filteredStuds=students.filter(student=>{
-        return student.name.includes(keyword);
+    state.search=searchBox.value;
+    updateUI();
     })
 
-    renderTable(filteredStuds);
+}
+
+//Department Filter
+function deptFilter(){
+const departmentBox=document.querySelector(".dept");
+
+departmentBox.addEventListener("change", ()=>{
+    state.department=departmentBox.value;
+    updateUI();
 })
 }
 
+//sorting function
+function sortCgpa(){
+    const sortBox=document.querySelector(".sort").querySelector("select")
+    sortBox.addEventListener("change", ()=>{
+        state.sort=sortBox.value;
+        updateUI();
+    })
+}
 
+
+//Table Renderer
 function renderTable(students){
     const t1body=document.querySelector('.student-table').querySelector('tbody');
     
@@ -47,7 +101,7 @@ function renderTable(students){
             <td>${student.phone}</td>
             <td>${student.department}</td>
             <td>${student.year}</td>
-            <td>${student.Cgpa}</td>
+            <td>${student.cgpa}</td>
             <td>${student.attendance}</td>
         `;
 
@@ -55,11 +109,17 @@ function renderTable(students){
 })
 }
 
-async function displayStudents(){
-const students=await loadStudents();
 
-renderTable(students);
-setupSearch(students);
+async function displayStudents(){
+
+//load students into state
+state.students=await loadStudents();
+
+//initial Render
+updateUI();
+setupSearch();
+deptFilter();
+sortCgpa();
 }
 
 
