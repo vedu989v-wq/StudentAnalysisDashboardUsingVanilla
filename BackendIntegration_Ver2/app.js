2b6e5db258f62b1238b49bb00960b20be1f5e71d
vedu989v-wq <vedu989v@gmail.com>
2026-07-07 20:17:01 +0530

    Designed and Connected Express Backend, created an Express API for fetching Student data, Implemented CORS middleware for seamless connection b/w frontend and backend

---
+++ BackendIntegration_Ver2/app.js
@@ -0,0 +1,126 @@
+async function loadStudents(){
+    try{
+        //Loading the JSON file
+        const response=await fetch("http://localhost:3000/");
+        console.log(response)
+        //convert JSON to JS object
+        const students=await response.json();
+        return students;
+
+        
+    } catch(error){
+        console.log("Error: ", error);
+    }
+
+    
+}
+
+//PCB of This Application: Store current State
+const state={
+    students:[],
+    search:"",
+    department:"ALL",
+    sort:"none"
+}
+
+function updateUI(){
+    let filtered=[...state.students];
+
+    //Search Function
+    if(state.search!==""){
+        filtered=filtered.filter(student=>student
+            .name.toLowerCase()
+            .includes(state.search.toLowerCase())
+        ); //for matching purpose
+    }
+
+    //Department
+    if(state.department!=='ALL'){
+        filtered=filtered.filter(student=>student.department==state.department);
+    }
+
+    
+
+    //sorting
+    if(state.sort!='none'){
+       if(state.sort=="cgpa-asc"){
+         filtered.sort((a,b)=>a.cgpa-b.cgpa);
+       }
+       if(state.sort=="cgpa-desc"){
+         filtered.sort((a,b)=>b.cgpa-a.cgpa);
+       }
+    }
+
+    renderTable(filtered)
+}
+
+//Search Filter
+function setupSearch(){
+
+const searchBox=document.querySelector('.search').querySelector('input')
+
+searchBox.addEventListener('input', ()=>{
+    state.search=searchBox.value;
+    updateUI();
+    })
+
+}
+
+//Department Filter
+function deptFilter(){
+const departmentBox=document.querySelector(".dept");
+
+departmentBox.addEventListener("change", ()=>{
+    state.department=departmentBox.value;
+    updateUI();
+})
+}
+
+//sorting function
+function sortCgpa(){
+    const sortBox=document.querySelector(".sort").querySelector("select")
+    sortBox.addEventListener("change", ()=>{
+        state.sort=sortBox.value;
+        updateUI();
+    })
+}
+
+
+//Table Renderer
+function renderTable(students){
+    const t1body=document.querySelector('.student-table').querySelector('tbody');
+    
+    t1body.innerHTML='';
+    students.forEach(student=>{
+    const row=document.createElement('tr');
+
+    row.innerHTML = `
+            <td>${student.studentid}</td>
+            <td>${student.name}</td>
+            <td>${student.email}</td>
+            <td>${student.phone}</td>
+            <td>${student.department}</td>
+            <td>${student.year}</td>
+            <td>${student.cgpa}</td>
+            <td>${student.attendance}</td>
+        `;
+
+        t1body.appendChild(row);
+})
+}
+
+
+async function displayStudents(){
+
+//load students into state
+state.students=await loadStudents();
+
+//initial Render
+updateUI();
+setupSearch();
+deptFilter();
+sortCgpa();
+}
+
+
+displayStudents();
---
+++ BackendIntegration_Ver2/database/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import pool from "./db.js";
+
+const app=express();
+
+app.use(cors()); //explicit permission given by server.js to frontend application to access 
+app.use(express.json()); //parses table rows to JSON objects
+
+app.get("/", async(req, res)=>{
+    try{
+        const result=await pool.query(
+            "SELECT * FROM studentinfo"
+        )
+
+    res.json(result.rows)
+    // console.log(result.rows)
+} catch(err){
+   console.log(err);
+
+   res.status(500).json({
+    error:"DataBase Error"
+   })
+
+   }
+})
+
+
+
+app.listen(3000, ()=>{
+    console.log(`Server running Flawlessly on port 3000 with link http://localhost:3000`)
+}) 
