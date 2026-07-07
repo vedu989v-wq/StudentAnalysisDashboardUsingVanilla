805859d00a1dc0c91952715c4990fdbb7953b015
Vedansh Sharma <vedu989v@gmail.com>
2026-07-08 01:27:24 +0530

    Update student data source to remote URL

--- student_dashboard/app.js
+++ student_dashboard/app.js
@@ -1,7 +1,7 @@
 async function loadStudents(){
     try{
         //Loading the JSON file
-        const response=await fetch('data/students.json');
+        const response=await fetch('https://studentanalysisbackend.vercel.app/');
         console.log(response)
         //convert JSON to JS object
         const students=await response.json();
